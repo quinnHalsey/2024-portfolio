d21bfc0d2291ff90a86ffc9563514cf9c65e3118
Halsey Quinn <halsey.e.quinn@gmail.com>
2024-09-10 18:29:05 -0400

    add left-aligned navigation with SVG dark mode toggle component

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // import Home from './Home';
 import Wyatt from './projects/Wyatt';
-import Toggle from './components/Toggle';
+import Navigation from './components/Navigation';
 
 import './App.css';
 
 const App = () => {
     return (
         <Router>
+            <Navigation />
             <Routes>
                 <Route path='/' element={<Wyatt />} />
                 <Route path='/wyatt' element={<Wyatt />} />
             </Routes>
-            <Toggle />
         </Router>
     );
 };
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,13 @@
+import Toggle from '../Toggle';
+
+import './Navigation.css';
+
+const Navigation = () => {
+    return (
+        <div className='navigation-wrapper'>
+            <Toggle />
+        </div>
+    );
+};
+
+export default Navigation;
