b8d9cb8fe77b538fcd2b836fe603ab8f82895107
Halsey Quinn <halsey.e.quinn@gmail.com>
2024-10-14 17:36:47 -0400

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
     ],
     output: {
         path: path.resolve(__dirname, 'dist/'),
-        publicPath: '/dist/',
+        publicPath: '/',
         filename: 'bundle.js',
     },
     devServer: {
